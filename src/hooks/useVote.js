8bce658cd190d17dafb709c4b24c9234cb805487
ipin0917826334 <71114672+ipin0917826334@users.noreply.github.com>
2023-04-17 19:54:25 +0700

    Update useVote.js

--- src/hooks/useVote.js
+++ src/hooks/useVote.js
@@ -51,7 +51,7 @@ const useVote = (initialLikes, initialDislikes, initialVote, commentId) => {
       try {
         const response = await api.put(`/topics/comments/${commentId}`, { action: "dislike", userId: userData._id });
         const updatedComment = response.data; // Assuming your backend returns the updated comment data
-        setDisLikes(updatedComment.dislikes); // Update the local state with the new likes count from the backend
+        setDislikes(updatedComment.dislikes); // Update the local state with the new likes count from the backend
     } catch (error) {
       console.error("Error updating likes:", error);
       }
